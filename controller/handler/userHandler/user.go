e72a911aac0a7d0f5bf67cac06f2926ec2946b35
Hamzah Abalfatah Alfauzi <alfauzi.hamzah@gmail.com>
2022-02-15 14:53:09 +0700

    handler get all users

--- controller/handler/userHandler/user.go
+++ controller/handler/userHandler/user.go
@@ -2,6 +2,7 @@ package userHandler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"sirclo/project-capstone/controller/service/userService"
 
@@ -16,6 +17,29 @@ func NewUserHandler(userService userService.UserServiceInterface) UserHandlerInt
 	return &userHandler{
 		userService: userService,
 	}
+}
+
+func (uh *userHandler) GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	// id := vars["id"]
+
+	users, err := uh.userService.GetUsers()
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("not found"))
+	}
+
+	// var data []userEntities.User
+	// for i, _ := range users {
+	// 	data = append(data, users[i])
+	// }
+	response, _ := json.Marshal(users)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+
 }
 
 func (uh *userHandler) GetUserHandler(w http.ResponseWriter, r *http.Request) {
