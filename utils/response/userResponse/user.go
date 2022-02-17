5d2edf19dd7b5bccc98082bad634f3da3fb4c518
justjundana <jalbasyir@gmail.com>
2022-02-18 05:56:09 +0700

    User Response - Formatter Auth & User

---
+++ utils/response/userResponse/user.go
@@ -0,0 +1,38 @@
+package userResponse
+
+import (
+	"sirclo/project-capstone/entities/userEntities"
+)
+
+type AuthFormatter struct {
+	Token string `json:"token"`
+}
+
+func FormatAuth(token string) AuthFormatter {
+	formatter := AuthFormatter{
+		Token: token,
+	}
+
+	return formatter
+}
+
+type UserFormatter struct {
+	ID       string `json:"id"`
+	Avatar   string `json:"avatar"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Phone    string `json:"phone"`
+}
+
+func FormatUser(user userEntities.User) UserFormatter {
+	formatter := UserFormatter{
+		ID:     user.ID,
+		Avatar: user.Avatar,
+		Email:  user.Email,
+		Name:   user.Name,
+		Phone:  user.Phone,
+	}
+
+	return formatter
+}
