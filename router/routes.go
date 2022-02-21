42b020e6e3c1a77f5824d573bd30ad0cfc342568
Hamzah Abalfatah Alfauzi <alfauzi.hamzah@gmail.com>
2022-02-21 16:46:50 +0700

    router days

--- router/routes.go
+++ router/routes.go
@@ -2,8 +2,10 @@ package router
 
 import (
 	"net/http"
+	"sirclo/project-capstone/repository/dayRepository"
 	"sirclo/project-capstone/repository/officeRepository"
 	"sirclo/project-capstone/repository/userRepository"
+	"sirclo/project-capstone/router/dayRouter"
 	"sirclo/project-capstone/router/officeRouter"
 	"sirclo/project-capstone/router/userRouter"
 	"strings"
@@ -14,10 +16,12 @@ import (
 func Routes(
 	userRepo userRepository.UserRepoInterface,
 	officeRepo officeRepository.OfficeRepoInterface,
+	dayRepo dayRepository.DayRepoInterface,
 ) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	mount(router, "/users", userRouter.UserResource{}.UserRoute(userRepo))
 	mount(router, "/offices", officeRouter.OfficeResource{}.OfficeRoute(officeRepo))
+	mount(router, "/days", dayRouter.DayResource{}.DayRoute(dayRepo))
 
 	return router
 }
