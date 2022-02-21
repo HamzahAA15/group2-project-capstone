12fe7a76943dbcebec8861ce338f0ec1cdce6f87
justjundana <jalbasyir@gmail.com>
2022-02-22 00:54:04 +0700

    Certificate Entities

---
+++ entities/certificateEntities/certificate.go
@@ -0,0 +1,17 @@
+package certificateEntities
+
+import (
+	"sirclo/project-capstone/entities/userEntities"
+	"time"
+)
+
+type Certificate struct {
+	ID        string
+	User      userEntities.User
+	Image     string
+	Dosage    int
+	Status    string
+	Admin     userEntities.User
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
