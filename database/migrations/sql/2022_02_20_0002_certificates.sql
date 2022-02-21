b5c4048457293146b3028edbee711245f0329a66
justjundana <jalbasyir@gmail.com>
2022-02-21 11:18:45 +0700

    Fixing Database

--- database/migrations/sql/2022_02_20_0002_certificates.sql
+++ database/migrations/sql/2022_02_20_0002_certificates.sql
@@ -5,8 +5,10 @@ CREATE TABLE certificates (
 	user_id VARCHAR(50) NOT NULL,
 	image VARCHAR(255) NOT NULL,
 	dosage INT NOT NULL,
-	status ENUM('approved', 'rejected') NOT NULL,
+	status ENUM('pending', 'approved', 'rejected') DEFAULT "pending",
+	admin_id VARCHAR(50) DEFAULT NULL,
 	created_at TIMESTAMP DEFAULT now(),
 	updated_at TIMESTAMP DEFAULT now(),
-	FOREIGN KEY (user_id) REFERENCES users(id)
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (admin_id) REFERENCES users(id)
 );
--- database/migrations/sql/2022_02_20_0005_checkins.sql
+++ database/migrations/sql/2022_02_20_0005_checkins.sql
@@ -5,5 +5,6 @@ CREATE TABLE checkins (
 	attendance_id VARCHAR(50) NOT NULL,
     temprature FLOAT NOT NULL,
 	checkin_time TIMESTAMP DEFAULT now() NOT NULL,
+	checkout_time TIMESTAMP DEFAULT NULL,
 	FOREIGN KEY (attendance_id) REFERENCES attendances(id)
 );
