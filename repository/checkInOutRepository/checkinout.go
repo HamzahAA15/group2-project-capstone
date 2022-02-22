96f24d819f47a469f4327c78f067fc71dc003645
justjundana <jalbasyir@gmail.com>
2022-02-22 22:02:44 +0700

    Interface and Query for GetCheckInsOuts by User

--- repository/checkInOutRepository/checkinout.go
+++ repository/checkInOutRepository/checkinout.go
@@ -43,3 +43,36 @@ func (cr *checkInOutRepo) Gets() ([]checkinEntities.Checkin, error) {
 
 	return checkinsouts, nil
 }
+
+func (cr *checkInOutRepo) GetByUser(userID string) ([]checkinEntities.Checkin, error) {
+	var checkinsouts []checkinEntities.Checkin
+
+	result, err := cr.db.Query(`
+	SELECT 
+		checkins.id, checkins.attendance_id, checkins.temprature, checkins.is_checkins, checkins.is_checkouts,checkins. created_at, checkins.updated_at
+	FROM 
+		checkins
+	JOIN
+		attendances ON attendances.id = checkins.attendance_id
+	WHERE
+		attendances.user_id = ?
+	ORDER BY 
+		created_at DESC`, userID)
+	if err != nil {
+		return checkinsouts, err
+	}
+
+	for result.Next() {
+		var checkinout checkinEntities.Checkin
+
+		errScan := result.Scan(&checkinout.ID, &checkinout.AttendanceID, &checkinout.Temprature, &checkinout.IsCheckIns, &checkinout.IsCheckOuts, &checkinout.CreatedAt, &checkinout.UpdatedAt)
+
+		if errScan != nil {
+			return checkinsouts, errScan
+		}
+
+		checkinsouts = append(checkinsouts, checkinout)
+	}
+
+	return checkinsouts, nil
+}
