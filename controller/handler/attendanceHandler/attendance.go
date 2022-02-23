70008f86e707c445aae6d2e43ef1df367300f389
Hamzah Abalfatah Alfauzi <alfauzi.hamzah@gmail.com>
2022-02-23 11:42:49 +0700

    handler update attendance

--- controller/handler/attendanceHandler/attendance.go
+++ controller/handler/attendanceHandler/attendance.go
@@ -41,11 +41,47 @@ func (ah *attHandler) CreateAttendance(w http.ResponseWriter, r *http.Request) {
 		w.Write(response)
 	default: // default response success
 		formatter := attendanceResponse.FormatAtt(attCreate)
-		response, _ := json.Marshal(utils.APIResponse("Success Create User Data", http.StatusOK, true, formatter))
+		response, _ := json.Marshal(utils.APIResponse("Success Create Attendace Data", http.StatusOK, true, formatter))
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(response)
 	}
 
+}
+
+func (ah *attHandler) UpdateAttendance(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user := middleware.ForContext(ctx)
+
+	checkUserRole := ah.attService.CheckUserRole(user.ID)
+	if checkUserRole != "admin" {
+		response, _ := json.Marshal(utils.APIResponse("You are not admin", http.StatusUnauthorized, false, nil))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write(response)
+		return
+	}
+
+	var input attendanceRequest.UpdateAttRequest
+	decoder := json.NewDecoder(r.Body)
+	_ = decoder.Decode(&input)
+
+	attUpdate, err := ah.attService.UpdateAttendance(user.ID, input)
+	switch {
+	case err != nil:
+		response, _ := json.Marshal(utils.APIResponse("Internal Server Error", http.StatusInternalServerError, false, nil))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(response)
+	default:
+		formatter := attendanceResponse.FormatUpdateAtt(attUpdate)
+		response, _ := json.Marshal(utils.APIResponse("Success Update Day Data", http.StatusOK, true, formatter))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	}
 }
