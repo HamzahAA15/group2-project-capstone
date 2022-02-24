791634f650d3231911f6130de61a33f9d4992cc7
justjundana <jalbasyir@gmail.com>
2022-02-24 21:17:49 +0700

    Interface and Query for CheckRequest and CheckData Attendance

--- repository/checkInOutRepository/interface.go
+++ repository/checkInOutRepository/interface.go
@@ -5,6 +5,8 @@ import "sirclo/project-capstone/entities/checkinEntities"
 type CheckInOutRepoInterface interface {
 	Gets() ([]checkinEntities.Checkin, error)
 	GetByUser(userID string) ([]checkinEntities.Checkin, error)
+	CheckRequest(userID string, attendanceID string) int
+	CheckData(userID string, attendanceID string) int
 	CheckIn(checkinout checkinEntities.Checkin) (checkinEntities.Checkin, error)
 	CheckOut(userID string, checkinout checkinEntities.Checkin) (checkinEntities.Checkin, error)
 }
