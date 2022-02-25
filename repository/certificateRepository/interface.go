bc09a4523d24b8c7b1cb02b0d9015eb6ae4918da
justjundana <jalbasyir@gmail.com>
2022-02-25 17:44:57 +0700

    Interface and Query for Count Vaccine si Approved and Get Vaccine Dose

--- repository/certificateRepository/interface.go
+++ repository/certificateRepository/interface.go
@@ -5,5 +5,7 @@ import "sirclo/project-capstone/entities/certificateEntities"
 type CertificateInterface interface {
 	GetCertificates(officeID string) ([]certificateEntities.Certificate, error)
 	GetCertificateUser(userID string) ([]certificateEntities.Certificate, error)
+	CountVaccineIsAccept(userID string, dossage int) int
+	GetVaccineDose(userID string) int
 	UploadCertificateVaccine(certificate certificateEntities.Certificate) (certificateEntities.Certificate, error)
 }
