4859f5a80fdecd48aeac9325110ac648808b2572
justjundana <jalbasyir@gmail.com>
2022-02-25 17:23:40 +0700

    Upload Certificate Vaccine

--- controller/service/certificateService/interface.go
+++ controller/service/certificateService/interface.go
@@ -1,8 +1,12 @@
 package certificateService
 
-import "sirclo/project-capstone/entities/certificateEntities"
+import (
+	"sirclo/project-capstone/entities/certificateEntities"
+	"sirclo/project-capstone/utils/request/certificateRequest"
+)
 
 type CertificateServiceInterface interface {
 	GetCertificates(officeId string) ([]certificateEntities.Certificate, error)
 	GetCertificateUser(userId string) ([]certificateEntities.Certificate, error)
+	UploadCertificateVaccine(userID string, input certificateRequest.CertificateUploadRequest) error
 }
