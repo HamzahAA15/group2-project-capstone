cb1f3eeaae8c1dd2b2c8ce701702f1196f52a8eb
Hamzah Abalfatah Alfauzi <alfauzi.hamzah@gmail.com>
2022-02-25 14:58:14 +0700

    tambah query param office & order by

--- repository/attendanceRepository/interface.go
+++ repository/attendanceRepository/interface.go
@@ -3,7 +3,7 @@ package attendanceRepository
 import "sirclo/project-capstone/entities/attendanceEntities"
 
 type AttendanceRepoInterface interface {
-	GetAttendances(employee, time, status string) ([]attendanceEntities.Attendance, error)
+	GetAttendances(employee, time, status, office, order string) ([]attendanceEntities.Attendance, error)
 	CreateAttendance(att attendanceEntities.Attendance) (attendanceEntities.Attendance, error)
 	UpdateAttendance(att attendanceEntities.Attendance) (attendanceEntities.Attendance, error)
 }
