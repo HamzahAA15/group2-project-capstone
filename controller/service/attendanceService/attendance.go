a350e6ebab941feed0a44729876afd5faf74cd9d
justjundana <jalbasyir@gmail.com>
2022-02-27 06:23:26 +0700

    Service Handler Get Attendances Request

--- controller/service/attendanceService/attendance.go
+++ controller/service/attendanceService/attendance.go
@@ -22,8 +22,8 @@ func NewAttendanceService(attRepo attendanceRepository.AttendanceRepoInterface, 
 	}
 }
 
-func (as *attendanceService) GetAttendances(employee, time, status, office, order string) ([]attendanceEntities.Attendance, error) {
-	attendances, err := as.attRepo.GetAttendances(employee, time, status, office, order)
+func (as *attendanceService) GetAttendances(employee, date, status, office, order string) ([]attendanceEntities.Attendance, error) {
+	attendances, err := as.attRepo.GetAttendances(employee, date, status, office, order)
 	if err != nil {
 		return attendances, err
 	}
