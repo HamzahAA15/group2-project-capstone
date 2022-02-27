4321a6d310ce92b37b0f671bb103ae70ba71963b
Hamzah Abalfatah Alfauzi <alfauzi.hamzah@gmail.com>
2022-02-27 07:24:43 +0700

    FIX bug get days

--- repository/dayRepository/day.go
+++ repository/dayRepository/day.go
@@ -19,17 +19,28 @@ func (dr *dayRepo) GetDays(office, time string) ([]dayEntities.Day, error) {
 	var days []dayEntities.Day
 	convOffice := "%" + office + "%"
 	convTime := "%" + time + "%"
-	result, err := dr.db.Query(`SELECT days.id, office.name, days.date, days.quota FROM days LEFT JOIN offices AS office ON office.id = days.office_id WHERE office.name LIKE ? AND days.date LIKE ?`, convOffice, convTime)
+
+	result, err := dr.db.Query(`
+	Select days.id, offices.name, days.date, days.quota,
+	count(attendances.day_id) AS total_approved, 
+	(days.quota-count(attendances.day_id)) AS remaining_quota 
+	FROM days
+    LEFT JOIN (SELECT attendances.day_id FROM attendances WHERE attendances.status = "approved") attendances ON attendances.day_id = days.id
+    LEFT JOIN offices ON offices.id = days.office_id
+    WHERE offices.id LIKE ? AND days.date LIKE ?
+    GROUP BY days.id ORDER BY days.date ASC`, convOffice, convTime)
+
 	if err != nil {
 		return days, err
 	}
 	for result.Next() {
 		var day dayEntities.Day
 
-		errScan := result.Scan(&day.ID, &day.OfficeId.Name, &day.Date, &day.Quota)
+		errScan := result.Scan(&day.ID, &day.OfficeId.Name, &day.Date, &day.Quota, &day.TotalApproved, &day.RemainingQuota)
 		if errScan != nil {
 			return days, errScan
 		}
+
 		days = append(days, day)
 	}
 
