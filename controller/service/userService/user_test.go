f346e31d2c6fe817dc20f23f6fa0a5ef02d93601
justjundana <jalbasyir@gmail.com>
2022-03-15 14:54:18 +0700

    Unit Test User with GoMock

--- controller/service/userService/user_test.go
+++ controller/service/userService/user_test.go
@@ -2,139 +2,273 @@ package userService
 
 import (
 	"errors"
-	"sirclo/project-capstone/entities/userEntities"
-	"sirclo/project-capstone/utils/request/userRequest"
-	"sirclo/project-capstone/utils/validation"
+	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	_userEntities "sirclo/project-capstone/entities/userEntities"
+	_mockRepo "sirclo/project-capstone/mocks/user/repository"
+	_utils "sirclo/project-capstone/utils"
+	_userRequest "sirclo/project-capstone/utils/request/userRequest"
+	_validation "sirclo/project-capstone/utils/validation"
+
+	gomock "github.com/golang/mock/gomock"
+	uuid "github.com/google/uuid"
+	require "github.com/stretchr/testify/require"
 )
 
+func TestLoginUser(t *testing.T) {
+	userData, password := randomUser(t)
+
+	testCases := []struct {
+		name     string
+		body     _userRequest.LoginUserInput
+		mockRepo func(user *_mockRepo.MockUserRepoInterface)
+	}{
+		{
+			name: "success",
+			body: _userRequest.LoginUserInput{
+				Identity: userData.Email,
+				Password: password,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().Login(gomock.Any()).Return(userData, nil).Times(1)
+			},
+		},
+		{
+			name: "error email",
+			body: _userRequest.LoginUserInput{
+				Identity: "error email",
+				Password: password,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().Login(gomock.Any()).Return(userData, errors.New("error email")).Times(1)
+
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mockRepo := _mockRepo.NewMockUserRepoInterface(ctrl)
+			tc.mockRepo(mockRepo)
+
+			s := NewUserService(mockRepo)
+			_, _ = s.LoginUserService(tc.body)
+		})
+	}
+}
+
 func TestGetUser(t *testing.T) {
-	userServiceMock := NewUserService(mockUserRepository{})
-	user, err := userServiceMock.GetUser("1")
-	assert.Nil(t, err)
-	assert.Equal(t, "hallo", user.Name, "nama tidak sama")
+	userData, _ := randomUser(t)
+
+	testCases := []struct {
+		name     string
+		idUser   string
+		mockRepo func(user *_mockRepo.MockUserRepoInterface)
+	}{
+		{
+			name:   "success",
+			idUser: "1",
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("1")).Return(userData, nil).Times(1)
+			},
+		},
+		{
+			name:   "error",
+			idUser: "2",
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("2")).Return(userData, errors.New("error")).Times(1)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mockRepo := _mockRepo.NewMockUserRepoInterface(ctrl)
+			tc.mockRepo(mockRepo)
+
+			s := NewUserService(mockRepo)
+			_, _ = s.GetUser(tc.idUser)
+		})
+	}
 }
 
 func TestCreateUser(t *testing.T) {
-	var create = userRequest.CreateUserInput{
-		ID:    "1",
-		Name:  "hallo",
-		Email: "mail@test.com",
+	userData, password := randomUser(t)
+
+	testCases := []struct {
+		name     string
+		body     _userRequest.CreateUserInput
+		mockRepo func(user *_mockRepo.MockUserRepoInterface)
+	}{
+		{
+			name: "success",
+			body: _userRequest.CreateUserInput{
+				Email:    userData.Email,
+				Password: password,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().CreateUser(gomock.Any()).Times(1).Return(userData, nil)
+			},
+		},
+		{
+			name: "error",
+			body: _userRequest.CreateUserInput{
+				Email:    "email",
+				Password: "password",
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().CreateUser(gomock.Any()).Times(1).Return(userData, errors.New("error"))
+			},
+		},
 	}
 
-	userServiceMock := NewUserService(mockUserRepository{})
-	user, err := userServiceMock.CreateUser(create)
-	assert.Nil(t, err)
-	assert.Equal(t, "1", user.ID, "nama tidak sama")
-	assert.Equal(t, "hallo", user.Name, "nama tidak sama")
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mockRepo := _mockRepo.NewMockUserRepoInterface(ctrl)
+			tc.mockRepo(mockRepo)
+
+			s := NewUserService(mockRepo)
+			_, _ = s.CreateUser(tc.body)
+		})
+	}
 }
 
-func TestLoginUserSuccess(t *testing.T) {
-	userServiceMock := NewUserService(mockUserRepository{})
-	input := userRequest.LoginUserInput{
-		Identity: "felicia@sirclo.com",
-		Password: "123456789qwerty",
+func TestUpdateUser(t *testing.T) {
+	userData, password := randomUser(t)
+
+	testCases := []struct {
+		name     string
+		idUser   string
+		body     _userRequest.UpdateUserInput
+		mockRepo func(user *_mockRepo.MockUserRepoInterface)
+	}{
+		{
+			name:   "success",
+			idUser: "1",
+			body: _userRequest.UpdateUserInput{
+				Password: password,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("1")).Return(userData, nil).Times(1)
+				user.EXPECT().UpdateUser(gomock.Any()).Return(userData, nil).Times(1)
+			},
+		},
+		{
+			name:   "Error UserID",
+			idUser: "2",
+			body: _userRequest.UpdateUserInput{
+				Password: password,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("2")).Return(userData, errors.New("error")).Times(1)
+			},
+		},
+		{
+			name:   "Error Update Data",
+			idUser: "3",
+			body: _userRequest.UpdateUserInput{
+				Password: password,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("3")).Return(userData, nil).Times(1)
+				user.EXPECT().UpdateUser(gomock.Any()).Return(userData, errors.New("error")).Times(1)
+			},
+		},
 	}
 
-	_, err := userServiceMock.LoginUserService(input)
-	assert.Nil(t, err)
-}
+	for i := range testCases {
+		tc := testCases[i]
 
-func TestLoginUserError(t *testing.T) {
-	userServiceMock := NewUserService(mockUserRepository{})
-	input := userRequest.LoginUserInput{
-		Identity: "",
-		Password: "123456789qwerty",
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mockRepo := _mockRepo.NewMockUserRepoInterface(ctrl)
+			tc.mockRepo(mockRepo)
+
+			s := NewUserService(mockRepo)
+			_, _ = s.UpdateUser(tc.idUser, tc.body)
+		})
 	}
-	_, err := userServiceMock.LoginUserService(input)
-	assert.NotNil(t, err)
 }
 
-func TestUpdateUserSuccess(t *testing.T) {
-	input := userRequest.UpdateUserInput{
-		Password: "passowrd123",
-	}
+func TestUploadAvatar(t *testing.T) {
+	userData, _ := randomUser(t)
 
-	userServiceMock := NewUserService(mockUserRepository{})
-	user, err := userServiceMock.UpdateUser("1", input)
-	assert.Nil(t, err)
-	assert.Equal(t, "1", user.ID, "tidak sama")
-}
-
-func TestUpdateUserError(t *testing.T) {
-	input := userRequest.UpdateUserInput{
-		Password: "passowrd123",
-	}
-
-	userServiceMock := NewUserService(mockUserRepository{})
-	_, err := userServiceMock.UpdateUser("2", input)
-	assert.NotNil(t, err)
-}
-
-func TestUploadAvatar1(t *testing.T) {
-	userServiceMock := NewUserService(mockUserRepository{})
-	_ = userServiceMock.UploadAvatarUser("1", "https://lazyevent.site")
-	assert.Equal(t, nil, nil)
-}
-
-func TestUploadAvatar2(t *testing.T) {
-	userServiceMock := NewUserService(mockUserRepository{})
-	_ = userServiceMock.UploadAvatarUser("2", "https://lazyevent.site")
-	assert.Equal(t, nil, nil)
-}
-
-type mockUserRepository struct{}
-
-func (m mockUserRepository) GetUser(id string) (userEntities.User, error) {
-	var gagal userEntities.User
-	if id == "2" {
-		return gagal, errors.New("error")
-	}
-
-	return userEntities.User{
-		ID:    "1",
-		Name:  "hallo",
-		Email: "mail@test.com",
-	}, nil
-}
-
-func (m mockUserRepository) CheckEmail(userChecked userEntities.User) (userEntities.User, error) {
-	return userEntities.User{
-		Email: userChecked.Email,
-	}, nil
-}
-
-func (m mockUserRepository) Login(identity string) (userEntities.User, error) {
-	if identity == "" {
-		return userEntities.User{}, errors.New("error")
+	testCases := []struct {
+		name     string
+		idUser   string
+		body     _userEntities.User
+		mockRepo func(user *_mockRepo.MockUserRepoInterface)
+	}{
+		{
+			name:   "success",
+			idUser: "1",
+			body: _userEntities.User{
+				Avatar: userData.Avatar,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("1")).Return(userData, nil).Times(1)
+				user.EXPECT().UploadAvatarUser(gomock.Any()).Return(nil).Times(1)
+			},
+		},
+		{
+			name:   "Error UserID",
+			idUser: "2",
+			body: _userEntities.User{
+				Avatar: userData.Avatar,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("2")).Return(userData, errors.New("error")).Times(1)
+			},
+		},
+		{
+			name:   "Error Upload Avatar",
+			idUser: "3",
+			body: _userEntities.User{
+				Avatar: userData.Avatar,
+			},
+			mockRepo: func(user *_mockRepo.MockUserRepoInterface) {
+				user.EXPECT().GetUser(gomock.Eq("3")).Return(userData, nil).Times(1)
+				user.EXPECT().UploadAvatarUser(gomock.Any()).Return(errors.New("error")).Times(1)
+			},
+		},
 	}
 
-	passwordHash, _ := validation.HashPassword("123456789qwerty")
-	return userEntities.User{
-		ID:       "1",
-		Email:    "felicia@sirclo.com",
-		Password: string(passwordHash),
-		Role:     "admin",
-	}, nil
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mockRepo := _mockRepo.NewMockUserRepoInterface(ctrl)
+			tc.mockRepo(mockRepo)
+
+			s := NewUserService(mockRepo)
+			_ = s.UploadAvatarUser(tc.idUser, tc.body.Avatar)
+		})
+	}
 }
 
-func (m mockUserRepository) CreateUser(user userEntities.User) (userEntities.User, error) {
-	return userEntities.User{
-		ID:    "1",
-		Name:  "hallo",
-		Email: "mail@test.com",
-	}, nil
-}
-func (m mockUserRepository) UpdateUser(user userEntities.User) (userEntities.User, error) {
-	return userEntities.User{
-		ID:    "1",
-		Name:  "hallo",
-		Email: "mail@test.com",
-	}, nil
-}
+func randomUser(t *testing.T) (user _userEntities.User, password string) {
+	password = _utils.RandomString(6)
+	hashedPassword, err := _validation.HashPassword(password)
+	require.NoError(t, err)
 
-func (m mockUserRepository) UploadAvatarUser(user userEntities.User) error {
-	return nil
+	user = _userEntities.User{
+		ID:       uuid.New().String(),
+		Avatar:   fmt.Sprintf("https://ui-avatars.com/api/?name=%s", _utils.RandomString(5)),
+		Name:     _utils.RandomString(5),
+		Email:    fmt.Sprintf(_utils.RandomString(6) + "@" + _utils.RandomString(4) + ".com"),
+		Password: hashedPassword,
+	}
+
+	return
 }
