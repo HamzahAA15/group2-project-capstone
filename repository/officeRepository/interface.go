3282932999e72fc823c6a17fa2028da6e6aec4e1
justjundana <jalbasyir@gmail.com>
2022-03-15 15:10:51 +0700

    Generate Mock Office for Testing

--- repository/officeRepository/interface.go
+++ repository/officeRepository/interface.go
@@ -4,6 +4,7 @@ import (
 	"sirclo/project-capstone/entities/officeEntities"
 )
 
+//go:generate mockgen --destination=./../../mocks/office/repository/mock_repository_office.go -source=interface.go
 type OfficeRepoInterface interface {
 	GetOffices() ([]officeEntities.Office, error)
 }
