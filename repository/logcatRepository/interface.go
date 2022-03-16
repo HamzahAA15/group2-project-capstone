77db77ece1e7c353642c159dab8d74001eda3acc
justjundana <jalbasyir@gmail.com>
2022-03-16 11:55:30 +0700

    Generate Mock Logcat for Testing

--- repository/logcatRepository/interface.go
+++ repository/logcatRepository/interface.go
@@ -2,6 +2,7 @@ package logcatRepository
 
 import "sirclo/project-capstone/entities/logcatEntities"
 
+//go:generate mockgen --destination=./../../mocks/logcat/repository/mock_repository_logcat.go -source=interface.go
 type LogcatRepoInterface interface {
 	CreateLogcat(lc logcatEntities.Logcat) (logcatEntities.Logcat, error)
 	GetLogcats() ([]logcatEntities.Logcat, error)
